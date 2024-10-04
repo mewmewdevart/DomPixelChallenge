9e81b04aa8a894a3ac7cfb17a12a411731b865bd
larih <mewmewdevart@gmail.com>
2024-10-04 19:00:51 -0300

    feat: add the footer

---
+++ dompixel-blog/app/componenets/organisms/footer/footer.tsx
@@ -0,0 +1,18 @@
+import { Container, Group, ActionIcon, rem } from "@mantine/core";
+
+import style from "./footer.module.css";
+
+export function Footer() {
+  return (
+    <div className={style.footer}>
+      <Container className={style.inner}>
+        <p>
+          Desenvolvido com muito ☕ por {' '}
+          <a href="https://linktr.ee/mewmewdevart" target="_blank">
+            Larissa Cristina Benedito
+          </a>
+        </p>
+      </Container>
+    </div>
+  );
+}
--- dompixel-blog/app/pages/home.tsx
+++ dompixel-blog/app/pages/home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Navbar } from '../componenets/organisms/navbar/navbar';
+import { Footer } from '../componenets/organisms/footer/footer';
 
 
 const HomePage: React.FC = () => {
   return (
     <div className='flex flex-col min-h-screen w-full h-[100vh] bg-brand-primary-10'>
         <Navbar/>
+        <h1>o</h1>
+        <Footer/>
     </div>
   );
 };
