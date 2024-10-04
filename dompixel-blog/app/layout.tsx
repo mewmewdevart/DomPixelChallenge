25ef315ee5795d4f0c36fd2a0f778b57684ffea8
larih <mewmewdevart@gmail.com>
2024-10-04 18:17:28 -0300

    feat: add the imports of mantine in the pages and mention of fon[

--- dompixel-blog/app/layout.tsx
+++ dompixel-blog/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import '@mantine/core/styles.css';
 import "./globals.css";
 
+// 18pt Fonts
 const interBlackItalic18 = localFont({
   src: "./fonts/Inter_18pt-BlackItalic.ttf",
   variable: "--font-inter-black-italic-18",
@@ -18,7 +20,28 @@ const interBold18 = localFont({
   src: "./fonts/Inter_18pt-Bold.ttf",
   variable: "--font-inter-bold-18",
 });
+const interItalic18 = localFont({
+  src: "./fonts/Inter_18pt-Italic.ttf",
+  variable: "--font-inter-italic-18",
+});
+const interLight18 = localFont({
+  src: "./fonts/Inter_18pt-Light.ttf",
+  variable: "--font-inter-light-18",
+});
+const interMedium18 = localFont({
+  src: "./fonts/Inter_18pt-Medium.ttf",
+  variable: "--font-inter-medium-18",
+});
+const interSemiBold18 = localFont({
+  src: "./fonts/Inter_18pt-SemiBold.ttf",
+  variable: "--font-inter-semi-bold-18",
+});
+const interThin18 = localFont({
+  src: "./fonts/Inter_18pt-Thin.ttf",
+  variable: "--font-inter-thin-18",
+});
 
+// 24pt Fonts
 const interBlackItalic24 = localFont({
   src: "./fonts/Inter_24pt-BlackItalic.ttf",
   variable: "--font-inter-black-italic-24",
@@ -35,7 +58,28 @@ const interBold24 = localFont({
   src: "./fonts/Inter_24pt-Bold.ttf",
   variable: "--font-inter-bold-24",
 });
+const interItalic24 = localFont({
+  src: "./fonts/Inter_24pt-Italic.ttf",
+  variable: "--font-inter-italic-24",
+});
+const interLight24 = localFont({
+  src: "./fonts/Inter_24pt-Light.ttf",
+  variable: "--font-inter-light-24",
+});
+const interMedium24 = localFont({
+  src: "./fonts/Inter_24pt-Medium.ttf",
+  variable: "--font-inter-medium-24",
+});
+const interSemiBold24 = localFont({
+  src: "./fonts/Inter_24pt-SemiBold.ttf",
+  variable: "--font-inter-semi-bold-24",
+});
+const interThin24 = localFont({
+  src: "./fonts/Inter_24pt-Thin.ttf",
+  variable: "--font-inter-thin-24",
+});
 
+// 28pt Fonts
 const interBlackItalic28 = localFont({
   src: "./fonts/Inter_28pt-BlackItalic.ttf",
   variable: "--font-inter-black-italic-28",
@@ -52,6 +96,26 @@ const interBold28 = localFont({
   src: "./fonts/Inter_28pt-Bold.ttf",
   variable: "--font-inter-bold-28",
 });
+const interItalic28 = localFont({
+  src: "./fonts/Inter_28pt-Italic.ttf",
+  variable: "--font-inter-italic-28",
+});
+const interLight28 = localFont({
+  src: "./fonts/Inter_28pt-Light.ttf",
+  variable: "--font-inter-light-28",
+});
+const interMedium28 = localFont({
+  src: "./fonts/Inter_28pt-Medium.ttf",
+  variable: "--font-inter-medium-28",
+});
+const interSemiBold28 = localFont({
+  src: "./fonts/Inter_28pt-SemiBold.ttf",
+  variable: "--font-inter-semi-bold-28",
+});
+const interThin28 = localFont({
+  src: "./fonts/Inter_28pt-Thin.ttf",
+  variable: "--font-inter-thin-28",
+});
 
 export const metadata: Metadata = {
   title: "DomPixel Blog",
@@ -67,9 +131,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`
-          ${interBlackItalic18.variable} ${interBlack18.variable} ${interBoldItalic18.variable} ${interBold18.variable}
-          ${interBlackItalic24.variable} ${interBlack24.variable} ${interBoldItalic24.variable} ${interBold24.variable}
-          ${interBlackItalic28.variable} ${interBlack28.variable} ${interBoldItalic28.variable} ${interBold28.variable}
+          ${interBlackItalic18.variable} ${interBlack18.variable} ${interBoldItalic18.variable} ${interBold18.variable} ${interItalic18.variable} ${interLight18.variable} ${interMedium18.variable} ${interSemiBold18.variable} ${interThin18.variable}
+          ${interBlackItalic24.variable} ${interBlack24.variable} ${interBoldItalic24.variable} ${interBold24.variable} ${interItalic24.variable} ${interLight24.variable} ${interMedium24.variable} ${interSemiBold24.variable} ${interThin24.variable}
+          ${interBlackItalic28.variable} ${interBlack28.variable} ${interBoldItalic28.variable} ${interBold28.variable} ${interItalic28.variable} ${interLight28.variable} ${interMedium28.variable} ${interSemiBold28.variable} ${interThin28.variable}
           antialiased`}
       >
         {children}
--- dompixel-blog/app/page.tsx
+++ dompixel-blog/app/page.tsx
@@ -1,11 +1,16 @@
 
 
 import React from 'react';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+import HomePage from '@pages/home';
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+    <MantineProvider>
+      <HomePage/>
+      </MantineProvider>
     </>
   );
 }
